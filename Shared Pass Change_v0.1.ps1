afc96ef134281b7000b609303e088b14119ec7bf
pidgu91 <54459016+pidgu91@users.noreply.github.com>
2022-01-23 10:54:41 -0600

    Update Shared Pass Change_v0.1.ps1

--- Shared Pass Change_v0.1.ps1
+++ Shared Pass Change_v0.1.ps1
@@ -1,6 +1,6 @@
 ﻿#runasadministrator
-$passwords = Import-Csv -Path C:\Users\Zach\Documents\pass.csv
-$computers = Import-Csv -Path C:\Users\zach\Documents\computers.csv
+$passwords = Import-Csv -Path C:\Temp\pass.csv
+$computers = Import-Csv -Path C:\Temp\computers.csv
 
 foreach ($computer in $computers){
     Invoke-Command -ComputerName $computer -ScriptBlock {Set-ItemProperty -Path "HKLM:\software\microsoft\windows nt\currentversion\winlogon" -Name "DefaultPassword" -Value $passwords}
