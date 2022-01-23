5dcb667b66506cccfeb72e12102e559be2533c7d
pidgu91 <54459016+pidgu91@users.noreply.github.com>
2022-01-23 10:45:17 -0600

    Add files via upload

---
+++ Shared Pass Change_v0.1.ps1
@@ -0,0 +1,14 @@
+﻿#runasadministrator
+$passwords = Import-Csv -Path C:\Users\Zach\Documents\pass.csv
+$computers = Import-Csv -Path C:\Users\zach\Documents\computers.csv
+
+foreach ($computer in $computers){
+    Invoke-Command -ComputerName $computer -ScriptBlock {Set-ItemProperty -Path "HKLM:\software\microsoft\windows nt\currentversion\winlogon" -Name "DefaultPassword" -Value $passwords}
+}
+
+
+
+
+
+
+
